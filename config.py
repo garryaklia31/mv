b0043022fe483fba69b7fca28620e2fe2c852211
garryaklia31 <158801619+garryaklia31@users.noreply.github.com>
2025-04-09 09:16:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,29 +2,29 @@ import os
 
 class Config(object):
     # Telegram Bot ka token
-    BOT_TOKEN = "7550452226:AAHmHKpLAElQNAqNIJnxrj8_Mfm339o3cL4"
+    BOT_TOKEN = "6478073504:AAFVjJYFBPo_Qk7sMLwjNVAKBNyexSipc8A"
     # Telegram API ki ID
     API_ID = 25773625
     # Telegram API ki hash key
     API_HASH = "137dc2742789fc81191a9bf7a25ff922"
     # Admin users ki IDs (comma se separate ki hui)
-    ADMIN = '7385324642'.split(',')
+    ADMIN = '1718738592'.split(',')
     # Admin IDs ko integer list mein convert karna
     ADMIN_ID = [int(id) for id in ADMIN]
     # MongoDB database ka URL
-    DB_URL = "mongodb+srv://mewadonlinestudy:yYjxKiZGQag8DnWH@cpvod.ywpecvo.mongodb.net/?retryWrites=true&w=majority&appName=cpvodak"
+    DB_URL = "mongodb+srv://harbanssidhu860:MEHAKPREET@cluster0.el4zawv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     # Database ka naam
     DB_NAME = "MY_BOT_DB"
     # Text log channel ki ID
-    TXT_LOG = -1002470171280
+    TXT_LOG = -1002637696162
     # Authentication log channel ki ID
-    AUTH_LOG = -1002470171280
+    AUTH_LOG = -1002637696162
     # Hit log channel ki ID
-    HIT_LOG = -1002470171280
+    HIT_LOG = -1002637696162
     # DRM dump channel ki ID
-    DRM_DUMP = -1002470171280
+    DRM_DUMP = -1002637696162
     # Main channel ki ID
-    CHANNEL = -1002470171280
+    CHANNEL = -1002637696162
     # Channel ka link
     CH_URL = "https://t.me/+sYOgy9ZgCZRhYTNl"
     # Bot ke owner ka Telegram link
